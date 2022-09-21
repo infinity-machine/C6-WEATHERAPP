eff62685172fd6bc86c1e77a35ef88a6fcdc1eea
Connor Carciofini <ccarciofini@gmail.com>
2022-09-21 14:46:53 -0500

    plz

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL = process.env.ATLAS_CONNECT_URL ? process.env.ATLAS_CONNECT_URL : 'mongodb://localhost:27017/weatherbot'
+const URL = process.env.ATLAS_CONNECT_URL ? process.env.ATLAS_CONNECT_URL : 'mongodb://localhost:27017/weatherbot';
 
 mongoose.connect(URL);
 
