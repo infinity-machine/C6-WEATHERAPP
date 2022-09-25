d69c9efcf63e67e2d4967b351f376c72d9f97e33
Connor Carciofini <ccarciofini@gmail.com>
2022-09-25 09:27:02 -0500

    fixed day issue

--- client/src/components/Forecast7Day.js
+++ client/src/components/Forecast7Day.js
@@ -8,6 +8,7 @@ const Forecast7Day = (props) => {
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${props.lat}&lon=${props.lon}&units=imperial&appid=${props.apiKey}`)
             .then(res => res.json())
             .then(data => {
+                console.log(data)
                 setWeatherData(data.daily);
             });
     }
@@ -15,10 +16,10 @@ const Forecast7Day = (props) => {
     useEffect(fetchWeather, [props.lat, props.lon]);
     return (
         <div>
-            {weatherData ? weatherData.map((data, index) => {
+            {weatherData ? weatherData.slice(1).map((data, index) => {
                 return <div key={index} className="centertext">
                     <div>
-                        <p>{moment().add(index, 'days').format('dddd, MMMM Do')}</p>
+                        <p>{moment().add(index + 1, 'days').format('dddd, MMMM Do')}</p>
                         <p>min: {data.temp.min}&#176;F max: {data.temp.max}&#176;F</p>
                         <p>humidity: {data.humidity}</p>
                         <p>windspeed: {data.wind_speed}</p>
